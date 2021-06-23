06ce88cc1e7e7c492347dd417a299bd5d4d759fd
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2021-06-23 11:29:05 -0300

    Update MarcaController.js

--- controllers/MarcaController.js
+++ controllers/MarcaController.js
@@ -5,5 +5,17 @@ module.exports = {
   async index(req, res) {
     const marcas = await knex("marcas").orderBy("nome");
     res.status(200).json(marcas);
+  },
+
+  async marcas_carros(req, res) {
+    const marcas = await knex
+      .select("m.nome")
+      .count("c.id as num")
+      .from("marcas as m")
+      .leftOuterJoin("carros as c", "m.id", "c.marca_id")
+      .groupBy("m.nome")
+      .having("num", ">", 0)
+
+    res.status(200).json(marcas);
   }
 };

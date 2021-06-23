4b0a3992b008064f04ac34836ac142c9f5619b7e
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2021-06-23 11:32:18 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -9,12 +9,15 @@ const CarroController = require('./controllers/CarroController')
 const UsuarioController = require('./controllers/UsuarioController')
 const login = require("./middleware/login");
 
-routes.get("/marcas", MarcaController.index);
+routes.get("/marcas", MarcaController.index)
+      .get("/marcas_carros", MarcaController.marcas_carros);
 
 routes.get("/carros", CarroController.index)
       .post("/carros", CarroController.store)
       .put("/carros/destaque/:id", CarroController.destaque)
       .get("/carros/destaques", CarroController.destaques)
+      .get("/carros/pesq/:palavra", CarroController.search)
+      .get("/carros/:id", CarroController.show)
       .delete("/carros/:id", CarroController.destroy)
 
 routes.get("/usuarios", UsuarioController.index)
